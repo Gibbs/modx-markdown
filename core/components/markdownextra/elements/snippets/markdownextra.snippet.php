82615e4312b11c25308065cafd0c21c5d8eb393b
Dan Gibbs <daniel.gibbs@gmail.com>
2013-08-01 00:07:31 +0100

    Added output filter

--- core/components/markdownextra/elements/snippets/markdownextra.snippet.php
+++ core/components/markdownextra/elements/snippets/markdownextra.snippet.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * MarkdownExtra plugin for MarkdownExtra extra
+ * MarkdownExtra snippet for MarkdownExtra extra
  *
  * Copyright Gold Coast Media 2013 by Dan Gibbs <dan@goldcoastmedia.co.uk>
- * Created on 07-27-2013
+ * Created on 07-31-2013
  *
  * MarkdownExtra is free software; you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
@@ -18,26 +18,33 @@
  * MarkdownExtra; if not, write to the Free Software Foundation, Inc., 59 Temple
  * Place, Suite 330, Boston, MA 02111-1307 USA
  *
- * @package    markdownextra
- * @author     Dan Gibbs  
+ * @package markdownextra
  */
 
-if($modx->context->key !== 'mgr' AND $modx->event->name == 'OnParseDocument')
-{
+// Output filter
+if( isset($input) ) {
+
 	require_once $modx->getOption('core_path') . 'components/markdownextra/vendor/markdown/Markdown.php';
 	require_once $modx->getOption('core_path') . 'components/markdownextra/vendor/markdown/MarkdownExtra.php';
-
-	$enabled  = $modx->getOption('markdownextra.enabled');
-	$mime_in  = $modx->getOption('markdownextra.mimemarkdown');
-	$mime_out = $modx->getOption('markdownextra.mimeout');
-
-	if($modx->resource->get('contentType') !== $mime_in OR !$enabled) return NULL;
-
+	
 	$output = NULL;
-
 	$markdown = new MarkdownExtra();
-	$output = $markdown::defaultTransform($modx->resource->getContent());
-
-	$modx->resource->setContent($output);
-	$modx->response->contentType->_fields['mime_type'] = $mime_out;
+	$output = $markdown::defaultTransform($input);
+	
+	return $output;
 }
+
+// Snippet
+if( array_change_key_case($scriptProperties, CASE_LOWER) AND isset($scriptProperties['content']) ) {
+
+	require_once $modx->getOption('core_path') . 'components/markdownextra/vendor/markdown/Markdown.php';
+	require_once $modx->getOption('core_path') . 'components/markdownextra/vendor/markdown/MarkdownExtra.php';
+	
+	$output = NULL;
+	$markdown = new MarkdownExtra();
+	$output = $markdown::defaultTransform($scriptProperties['content']);
+	
+	return $output;
+}
+
+return NULL;
